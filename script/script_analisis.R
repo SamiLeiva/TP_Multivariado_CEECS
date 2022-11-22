01d51227711ff08535ecb501bb1190d661c9bdc8
unknown <samantaleiva1@gmail.com>
2022-11-22 12:43:21 -0300

    Cambios en el script

--- script/script_analisis.R
+++ script/script_analisis.R
@@ -1,7 +1,9 @@
 library(corrplot)
 library(psych)
 
-### Conociendo la base para el TP ####
+########################################
+### Apertura de la base y selección ####
+########################################
 
 ## Uso de una base de datos abierta de 
  # Spreng, R. N. (2022, September 14). Goal-Directed Cognition in Older and Younger Adults. https://doi.org/10.17605/OSF.IO/YHZXE
@@ -14,39 +16,56 @@ datos <- read.csv2("https://osf.io/download/huj8p/",
 colnames(datos)
 datos_selec <- datos[,-c(1:9, 12:31, 37:50, 55,57,59)]
 
-# Estandarización de los datos: todas las variables, excepto algunas cognitivas, están expresadas en puntaje burto. Se estandarizan para haceras comparables.
-# Las variables 37 a 53 están en puntaje estandarizado T. Los paso a Z
+#######################
+### Estandarización ###
+#######################
+
+# Todas las variables, excepto algunas, están expresadas en puntaje burto. Se estandarizan para hacerlas comparables para el análisis de componentes principales.
+# Las variables 12 a 25 están expresadas en puntaje T. Los paso a Z.
 estandarizacionT <- function(x){
   (x-50)/10
 }
 datos_selec[12:25] <- apply(datos_selec[12:25], 2, estandarizacionT)
-# El resto están en puntaje bruto. Estandarizo en función de su media y DE
+# El resto están expresadas en puntaje bruto. Estandarizo en función de su media y DE
 estandarizacion <- function(x){
   (x-mean(x, na.rm = TRUE))/sd(x, na.rm=TRUE)
 }
 datos_selec[1:11] <- apply(datos_selec[1:11], 2, estandarizacion)
 
-#Unificación de variable depresión
-datos_selec$depre <- ifelse(is.na(datos_selec$gds_sum),
-                          datos_selec$bdi_sum,
-                          datos_selec$gds_sum)
-datos.pca <- datos_selec[,-c(1,2)] # Elimino bdi y gds
+#################################################
+### Puesta a punto final antes de aplicar PCA ###
+#################################################
 
-#Elimino los datos faltantes
+# Unificación de variable depresión: la sintomatología depresiva se evaluó con dos escalas diferentes (una aplicada a jovenes y otra a personas de mayor edad).
+# Dado que cada individuo tiene un solo valor para depresión (BDI o GDS), se unifica en una sola variable llamada "depre".
+datos_selec$depre <- ifelse(is.na(datos_selec$gds_sum),
+                            datos_selec$bdi_sum,
+                            datos_selec$gds_sum)
+datos.pca <- datos_selec[,-c(1,2)] # Elimino las columnas de bdi y gds
+
+# Se eliminan a los participantes con datos faltanes ya que PCA puede ser aplicado con datos completos. En este caso y para el TP de la metaria, se decide no hacer imputación de los casos.
 datos.pca <- na.omit(datos.pca)
 
-#Cambio los nombres de columnas a castellano
+#Cambio los nombres de columnas al castellano
 names(datos.pca) <- c("P_apertura", "P_responsabilidad","P_extraversion","P_amabilidad","P_neuroticismo",
                          "IRI_toma_perspectiva","IRI_fantasia","IRI_preocupacion_empatica","IRI_malestar_personal",
                          "E_ostilidad_desconfianza","E_apoyo_emocional","E_miedo_activacion","E_amistad",
                          "E_satisfaccion_de_vida","E_apoyo_instrumental","E_soledad","E_proposito","E_hostilidad_percibida",
                          "E_rechazo_percibido","E_estres_percibido","E_afecto_positivo","E_tristeza","E_autoeficacia","Depresion")
 
-#Correlaciones
-cortest.bartlett(cor(datos.pca), n = nrow(datos.pca))
+
+#################################################
+### Exploración de asociación entre variables ###
+#################################################
+
+# Gráficos de correlaciones entre las variables
+
 
 
 corrplot(cor(datos.pca), method = "ellipse", type = "upper")
+
+
+cortest.bartlett(cor(datos.pca), n = nrow(datos.pca))
 
 cp <- princomp(datos.pca)
 summary(cp)
